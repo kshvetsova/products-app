c396d8f3e135280b9697a18676788769df80333f
kshvetsova <katshvetsova@gmail.com>
2021-07-19 09:13:45 +0300

    Initial commit

--- src/components/NewProduct/NewProduct.jsx
+++ src/components/NewProduct/NewProduct.jsx
@@ -197,7 +197,7 @@ export const NewProduct = () => {
                 });
               }}
             >
-              <span className="button-plus">+</span>
+             <span>+</span>
             </button>
           </div>
           <button
